2d6314a78eeb6caf770f8badce5d01a54e59a3ce
mohamedlazrak2008-cpu <mohamedlazrak2008@gmail.com>
2026-02-05 15:10:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'dama-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://cdn-icons-png.flaticon.com/512/3211/3211231.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
+
